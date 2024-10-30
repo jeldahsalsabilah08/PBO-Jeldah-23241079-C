598a0ad00f9e8cc416c19c8acb5ab2b3707cf28f
jeldahsalsabilah08 <147676414+jeldahsalsabilah08@users.noreply.github.com>
2024-10-30 09:55:40 +0800

    Add files via upload

---
+++ MODUL 1/01-PengenalanClass&Objeck/java/src/App.java
@@ -0,0 +1,25 @@
+class Mahasiswa {
+    String Nama;
+    String NIM;
+    String Prodi;
+    Integer umur;
+    Double IPK;
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Mahasiswa mhs_1 = new Mahasiswa();
+        mhs_1.Nama = "jeldah";
+        mhs_1.NIM = "23241079";
+        mhs_1.Prodi = "Pendidikan Teknologi Informasi";
+        mhs_1.umur = 15;
+        mhs_1.IPK = 4.0;
+
+        System.out.println("Nama : " + mhs_1.Nama);
+        System.out.println("NIM : " + mhs_1.NIM);
+        System.out.println("Prodi : " + mhs_1.Prodi);
+        System.out.println("Umur : " + mhs_1.umur);
+        System.out.println("IPK : " + mhs_1.IPK);
+    }
+}
---
+++ MODUL 1/03-latihan/java/src/KelasMotorku.java
@@ -0,0 +1,29 @@
+class MOTOR {
+
+    String merk;
+    int tahun;
+    String noPolisi;
+    String warna;
+
+    public MOTOR(String merk, int tahun, String noPolisi, String warna) {
+        this.merk = merk;
+        this.tahun = tahun;
+        this.noPolisi = noPolisi;
+        this.warna = warna;
+    }
+
+    void showInfoMotor() {
+        System.out.println("Merk: " + this.merk);
+        System.out.println("Tahun: " + this.tahun);
+        System.out.println("No Polisi: " + this.noPolisi);
+        System.out.println("Warna: " + this.warna);
+    }
+}
+
+public class KelasMotorku {
+
+    public static void main(String[] args) {
+        MOTOR motorku = new MOTOR("Honda GL Pro", 1997, "G 5879 BF", "Hitam");
+        motorku.showInfoMotor();
+    }
+}
